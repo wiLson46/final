844061b9f90ee9ac7535dcf0479d16003d885dfb
W <31364471@terciariourquiza.edu.ar>
2024-09-30 20:24:33 -0300

    comentario

--- login.php
+++ login.php
@@ -5,5 +5,6 @@ $loguear = new Login($_POST['usuario'], $_POST['password']);
 
 $validar = $loguear->validarLogin($_POST['usuario'], $_POST['password']);
 
+//no se si esto es necesario
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($validar);
