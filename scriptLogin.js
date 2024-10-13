eb77311ba582bff1b07bdc86766aaeafdc1e07d3
W <31364471@terciariourquiza.edu.ar>
2024-10-13 12:35:49 -0300

    no pude hacer andar el showdiv

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,12 +1,20 @@
+console.log("entra 1");
+
+document.addEventListener("DOMContentLoaded", function () {
+    ocultar_divs();
+});
+
 function ocultar_divs() {
+    console.log("entra 2");
     document.querySelector('#loginSuccess').hidden = true;
     document.querySelector('#loginFail').hidden = true;
 }
 
 function mostrar_div(id_div) {
     ocultar_divs();
-    if (id_div == "lista") {
-        consultar_lista_empleados();
+    if (id_div == "loginSuccess") {
+        document.querySelector('#' + id_div).hidden = false;
+    } else {
+        document.querySelector('#' + id_div).hidden = false;
     }
-    document.querySelector('#' + id_div).hidden = false;
 }
